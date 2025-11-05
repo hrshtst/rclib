dc7b2fc276000a21736a0b140a8fc0d7faec9ea8
Hiroshi Atsuta <atsuta@ieee.org>
2025-11-05 19:08:38 +0900

    feat: Implement and test NvarReservoir class

--- cpp_core/include/rcl/reservoirs/NvarReservoir.h
+++ cpp_core/include/rcl/reservoirs/NvarReservoir.h
@@ -1,15 +1,22 @@
 #pragma once
 
 #include "rcl/Reservoir.h"
+#include <Eigen/Dense>
 
 class NvarReservoir : public Reservoir {
 public:
-    NvarReservoir(); // Add parameters later
+    NvarReservoir(int num_lags);
 
     Eigen::MatrixXd& advance(const Eigen::MatrixXd& input) override;
     void resetState() override;
     const Eigen::MatrixXd& getState() const override;
 
 private:
+    void initialize(int input_dim);
+
+    int num_lags;
+    int input_dim;
+    bool initialized;
     Eigen::MatrixXd state;
+    Eigen::MatrixXd past_inputs;
 };
