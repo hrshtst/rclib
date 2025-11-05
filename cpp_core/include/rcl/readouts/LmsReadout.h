123d0d04ceef87370d09fbf713e7b74fea546e2d
Hiroshi Atsuta <atsuta@ieee.org>
2025-11-05 19:22:05 +0900

    feat: Implement and test LmsReadout class

--- cpp_core/include/rcl/readouts/LmsReadout.h
+++ cpp_core/include/rcl/readouts/LmsReadout.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include "rcl/Readout.h"
+#include <Eigen/Dense>
 
 class LmsReadout : public Readout {
 public:
-    LmsReadout(); // Add parameters later
+    LmsReadout(double learning_rate = 0.01, bool include_bias = true);
 
     void fit(const Eigen::MatrixXd& states, const Eigen::MatrixXd& targets) override;
     void partialFit(const Eigen::MatrixXd& state, const Eigen::MatrixXd& target) override;
     Eigen::MatrixXd predict(const Eigen::MatrixXd& states) override;
 
 private:
-    Eigen::MatrixXd W_out;
+    double learning_rate;
+    bool include_bias;
+
+    Eigen::MatrixXd W_out; // Weight matrix
+    bool initialized;
 };
